4af9dcac7eb6221e0eec6e7ac95f1962386e766c
Ivan <ivanwyf@foxmail.com>
2020-07-31 19:13:31 +0800

    modify something

---
+++ naive_bayes/naive_bayes.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+class NaiveBayes:
+    def __init__(self):
+        pass
+
+    def load_dataset(self):
+        dataset=[['']]
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ svm/svm.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+class SVM:
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
